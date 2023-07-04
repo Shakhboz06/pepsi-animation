e91ce278c01a6e2f535f95a574f6d42d5b75bf1e
Shakhboz06 <shakhboz.shodiev2001@gmail.com>
2023-07-05 00:37:08 +0200

    my anima

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,39 @@
-const gulp = require('gulp')
-const autoprefixer = require('gulp-autoprefixer') // prefixex
-const cleanCSS = require('gulp-clean-css') // minify css
-const concat = require('gulp-concat') // unit files
-const sass = require('gulp-sass') // scss sass
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass')(require('sass'));
+const sassCompiler = require('sass');
 
 // sources
 const paths = {
     styles: {
         src: {
-            // Бери отсюда
-            custom: './styles/scss/*.scss'
+            // Get source files from here
+            custom: './Styles/scss/**/*.scss'
         },
-        // Ставь сюда
-        dist: './styles/css/'
+        // Place compiled files here
+        dist: './Styles/css/'
     }
-}
-
-sass.compiler = require('node-sass');
+};
 
 // styles custom optimize
 function CustomStyles() {
     return gulp.src(paths.styles.src.custom)
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ compiler: sassCompiler }).on('error', sass.logError))
         .pipe(concat('style.css'))
         .pipe(autoprefixer({ overrideBrowserslist: ['> 3%'], cascade: false }))
         .pipe(cleanCSS({ level: 2 }))
-        .pipe(gulp.dest(paths.styles.dist))
+        .pipe(gulp.dest(paths.styles.dist));
 }
 
-// watchin' onchange
+// watch for changes
 function watch() {
-    gulp.watch(paths.styles.src.custom, CustomStyles)
+    gulp.watch(paths.styles.src.custom, CustomStyles);
 }
 
-gulp.task('styles', CustomStyles)
-gulp.task('watch', watch)
+gulp.task('styles', CustomStyles);
+gulp.task('watch', watch);
 
-gulp.task('build', gulp.parallel(CustomStyles))
-gulp.task('default', gulp.series('build', 'watch'))
+gulp.task('build', gulp.parallel(CustomStyles));
+gulp.task('default', gulp.series('build', 'watch'));
